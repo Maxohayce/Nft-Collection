09970ae3d2f36806dc87e0dea5d6126a5556e091
Maxohayce <devmax.corl@gmail.com>
2022-07-22 02:27:01 +0100

    First collection

---
+++ crypto-devs/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = "0xF2620Aa69688Ebe2d1422873b2B35Ca592e5198C";
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = "https://nft-collection-sneh1999.vercel.app/api/";
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL };
